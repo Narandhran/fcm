e4bf14d0afd92789cc7f61c290c3010a0a50cd0e
narandhran <narandhran@gmail.com>
2018-07-16 15:37:46 +0530

    initial

---
+++ fcm_android/src/main/java/com/appraamlabs/FcmAndroidApplication.java
@@ -0,0 +1,12 @@
+package com.appraamlabs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FcmAndroidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FcmAndroidApplication.class, args);
+	}
+}
